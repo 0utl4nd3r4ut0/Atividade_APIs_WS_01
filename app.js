ed85f6366394b32e4f458b6232ef9d0e57e71a07
0utl4nd3r4ut0 <renandias.auto@gmail.com>
2023-10-22 01:40:55 -0300

    update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const https = require('https');
 const fs = require('fs')
+const crypto = require('crypto');
+const { json } = require("express/lib/response");
 const port = 3000;
 var app = express();
 
@@ -22,13 +24,26 @@ const credentials = {
   
 const server = https.createServer(credentials, app);
 
+function ETagCalc(resource){
+    const hash = crypto.createHash('sha1').update(JSON.stringify(resource)).digest('hex');
+    return `"${hash}"`
+}
+
 app.get('/:resource', (req, res) => {
     const resource = req.params.resource;
 
-    if(resultados[resource]){
-        res.json(resultados[resource]);
+    if (!resultados[resource]){
+        res.status(404).send('Recurso não encontrado');
+        return;
+    }
+    const resourceData = resultados[resource];
+    const etag = ETagCalc(resourceData);
+
+    if(req.headers['if-none-match'] === etag){
+        res.status(304).send();
     }else{
-        res.status(404).send('Recurso não encontrado!');
+        res.setHeader('ETag', etag);
+        res.json(resourceData)
     }
     });
 
